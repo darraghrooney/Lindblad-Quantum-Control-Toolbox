9e64832651a519430b8602434597703e765e9a39
Patrick <darraghrooney@gmail.com>
2015-11-17 16:43:39 -0800

    Added n.from.h function to horizon.find.R, also changed underscores to periods.

--- n=2_Data_Project/horizon.find.R
+++ n=2_Data_Project/horizon.find.R
@@ -6,7 +6,7 @@
 # horizon, the next three are the co-ordinates of the horizon, and the fifth is the
 # horizon magnitude.
 
-horizon_find <- function(a, b){
+horizon.find <- function(a, b){
   
   horizon = rep(0,5) 
 
@@ -34,7 +34,7 @@ horizon_find <- function(a, b){
       # The standard solution involves solving a cubic polynomial which we 
       # assign to a helper function. The data is re-scaled so that a1=1. This
       # avoids very large polynomial co-efficients.
-      mus = max(a)*find_mus3(a[c(J1,J2,J3)]/max(a),b[c(J1,J2,J3)]/max(a))
+      mus = max(a)*find.mus3(a[c(J1,J2,J3)]/max(a),b[c(J1,J2,J3)]/max(a))
       for (k in 1:length(mus)){
         nM = b/2/(-mus[k]+sum(a)-a)
         if (min(is.finite(nM))==0){next}
@@ -62,7 +62,7 @@ horizon_find <- function(a, b){
   # In general, we must solve a quintic polynomial
   else{
     # Use helper function to solve polynomial. Rescale to avoid numeric craziness
-    mus = max(a)*find_mus5(a/max(a),b/max(a))
+    mus = max(a)*find.mus5(a/max(a),b/max(a))
     
     # See which solution is best
     for (k in 1:length(mus)){
@@ -78,12 +78,12 @@ horizon_find <- function(a, b){
 }
 
 # Helper function to matrixify vector for cross product
-cross_mat <- function(hv){
-  hm = diag(rep(0,3))
-  hm[1,2] = hv[3] 
-  hm[2,3] = hv[1] 
-  hm[3,1] = hv[2] 
-  return( - hm + t(hm) )
+cross.mat <- function(vc){
+  cmt = diag(rep(0,3))
+  cmt[1,2] = vc[3] 
+  cmt[2,3] = vc[1] 
+  cmt[3,1] = vc[2] 
+  return( - cmt + t(cmt) )
 }
 
 # Calculates the Lagrangian multiplier for a Lindblad system
@@ -91,7 +91,7 @@ cross_mat <- function(hv){
 # It is expected that A1 is on the order of 100 ... coefficients
 # are re-scaled to maintain accuracy
 
-find_mus3 <- function(a,b){
+find.mus3 <- function(a,b){
   
   A1 = a[1]
   A2 = a[2]
@@ -118,10 +118,10 @@ find_mus3 <- function(a,b){
 # It is expected that max(a) is on the order of 100, as polynomial
 # co-efficients are re-scaled to maintain accuracy
 
-find_mus5 <- function(a,b){
+find.mus5 <- function(a,b){
 
   # Calculate coefficients, including re-scaling factors
-  coeff_exp = matrix(0,6,3);
+  coeff.exp = matrix(0,6,3);
   for(j in 1:3){
     j1 = (j %% 3) + 1;
     j2 = ((j+1) %% 3)+1;
@@ -129,15 +129,15 @@ find_mus5 <- function(a,b){
     A2 = a[j1]; B2 = b[j1];
     A3 = a[j2]; B3 = b[j2];
 
-    coeff_exp[1,j] = 2;   
-    coeff_exp[2,j] = -4*(A2+A3);   
-    coeff_exp[3,j] = 2*(A2^2+A3^2+4*A2*A3);   
-    coeff_exp[4,j] = -4*(A2*A3^2+A3*A2^2);   
-    coeff_exp[5,j] = 2*A2^2*A3^2;   
-    coeff_exp[2:6,j] = coeff_exp[2:6,j] - coeff_exp[1:5,j]*(A2/2+A3/2+A1);
-    coeff_exp[,j] = coeff_exp[,j]*B1^2;
+    coeff.exp[1,j] = 2;   
+    coeff.exp[2,j] = -4*(A2+A3);   
+    coeff.exp[3,j] = 2*(A2^2+A3^2+4*A2*A3);   
+    coeff.exp[4,j] = -4*(A2*A3^2+A3*A2^2);   
+    coeff.exp[5,j] = 2*A2^2*A3^2;   
+    coeff.exp[2:6,j] = coeff.exp[2:6,j] - coeff.exp[1:5,j]*(A2/2+A3/2+A1);
+    coeff.exp[,j] = coeff.exp[,j]*B1^2;
   }
-  coeff = rowSums(coeff_exp)
+  coeff = rowSums(coeff.exp)
   
   # Solve polynomial and scale back
   mus = sum(a) - polyroot(coeff[6:1])
@@ -149,7 +149,7 @@ find_mus5 <- function(a,b){
 
 # Helper function that calculates the components of b
 # from its angle parametrization
-b_from_angles <- function(a,cosphi,theta,B){
+b.from.angles <- function(a,cosphi,theta,B){
   
   b=rep(0,3)
   b[1] = 2*sqrt(a[2]*a[3])*B*cos(theta)*sin(acos(cosphi))
@@ -161,15 +161,30 @@ b_from_angles <- function(a,cosphi,theta,B){
 
 # Given a point in a Bloch ball for a Lindblad system, computes
 # the necessary (non-unique) Hamiltonian
-h_from_n <- function(n,a,b){
+h.from.n <- function(n,a,b){
   
   # Matrixify n
-  n_mat = cross_mat(n)
+  n.mat = cross.mat(n)
   
   # Compute Hamiltonian
   if (sum(n^2) < 1e-8){
     return(rep(0,3))
   }
-  hM = - n_mat %*% ( b + diag(a) %*% n)/(sum(n^2))
+  hM = - n.mat %*% ( b + diag(a) %*% n)/(sum(n^2))
   return(hM)
+}
+
+# Given a Hamiltonian and Lindblad system, return the asymptotic n.
+# If the system is degenerate, return FALSE.
+n.from.h <- function(h,a,b){
+  
+  # Compute dynamic matrix
+  fullmat = cross.mat(h) + diag(a - sum(a))
+  
+  if (det(fullmat)==0){     # In case of degeneracy
+    return(FALSE)
+  }
+  else{
+    return( solve(fullmat, -b) )  # Solve for asymptotic n
+  }
 }
