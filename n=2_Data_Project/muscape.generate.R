e12f306096b2d9149f89c0e43a5683650d5bb523
Patrick <darraghrooney@gmail.com>
2015-11-26 14:03:38 -0800

    Changed underscores to periods in muscape.generate.R and A.breakdown.R

--- n=2_Data_Project/muscape.generate.R
+++ n=2_Data_Project/muscape.generate.R
@@ -7,15 +7,15 @@
 # variables: cos(phi) and theta, where b1=sqrt(a2a3)*sin(theta)*cos(phi), 
 # b2 = sqrt(a1a3)*sin(phi)*sin(theta) and b3 = sqrt(a1a2)*cos(phi).
 
-# Input values are a_disc and ang_disc which discretize the ranges a2,a3 in [0,100] for
-# a_disc, and cp in [0,1] and theta in [0,pi/2] for ang_disc. The entire muscape will
+# Input values are a.disc and ang.disc which discretize the ranges a2,a3 in [0,100] for
+# a.disc, and cp in [0,1] and theta in [0,pi/2] for ang.disc. The entire muscape will
 # be generated in pieces, so we allow a2.inds and a3.inds to specify which values are
-# done. Note that a2.inds must be in (0,100]/a_disc and a3.inds in [0,a2.inds].
+# done. Note that a2.inds must be in (0,100]/a.disc and a3.inds in [0,a2.inds].
 
 # The resulting data frame contains the parameters, the mu, the corresponding 
 # horizon co-ordinates, and the horizon magnitude
 
-muscape_generate <- function(a_disc,ang_disc,a2.inds,a3.inds){
+muscape.generate <- function(a.disc,ang.disc,a2.inds,a3.inds){
   
   # Figure out how many rows will be in the data frame
   ac = 0
@@ -24,31 +24,31 @@ muscape_generate <- function(a_disc,ang_disc,a2.inds,a3.inds){
       ac = ac+1
     }
   }
-  no_pts = ang_disc^2*ac
+  no.pts = ang.disc^2*ac
   
   # Initialize data frame
-  muscape = data.frame(a2=double(no_pts),a3=double(no_pts),cp=double(no_pts),
-         th=double(no_pts),mu=double(no_pts),nM1=double(no_pts),nM2=double(no_pts),
-         nM3=double(no_pts),nMmag=double(no_pts))
+  muscape = data.frame(a2=double(no.pts),a3=double(no.pts),cp=double(no.pts),
+         theta=double(no.pts),mu=double(no.pts),nM1=double(no.pts),nM2=double(no.pts),
+         nM3=double(no.pts),nMmag=double(no.pts))
   count = 1
 
-  # We need the functions horizon_find() and b_from_angles()
-  source("horizon_find.R")
+  # We need the functions horizon.find() and b.from.angles()
+  source("horizon.find.R")
   
   # Sweep over a2, a3 indices
   for (j1 in a2.inds){
     for (j2 in a3.inds){
-      a=c(100,100/a_disc*j1,100/a_disc*j2)
+      a=c(100,100/a.disc*j1,100/a.disc*j2)
       
       # Sweep over angles
-      for (j3 in 0:ang_disc){
-        for (j4 in 0:ang_disc){
+      for (j3 in 0:ang.disc){
+        for (j4 in 0:ang.disc){
 
           # Calculate data
-          cp = j3/ang_disc
-          th = j4/ang_disc/2*pi
-          b= b_from_angles(a,cp,th,1)
-          mus = horizon_find(a,b)
+          cp = j3/ang.disc
+          th = j4/ang.disc/2*pi
+          b= b.from.angles(a,cp,th,1)
+          mus = horizon.find(a,b)
           muscape[count,] = c(a[2],a[3],cp,th,mus)
           count = count+1
         }
@@ -62,7 +62,7 @@ muscape_generate <- function(a_disc,ang_disc,a2.inds,a3.inds){
 # This function takes (pieces of) the original muscape, and calculates the necessary
 # Hamiltonian for each horizon. These Hamiltonians are added to the data frame.
 
-muscape_add_hams <- function(msc){
+muscape.add.hams <- function(msc){
 
   # Determine size of muscape piece
   nrows=dim(msc)[1]
@@ -78,8 +78,8 @@ muscape_add_hams <- function(msc){
     # Calculate necessary data and insert Hamiltonian into data frame
     msrow = as.matrix(msc[j,])
     a = as.vector(c(100, msrow[1], msrow[2]))
-    b = b_from_angles(a, msrow[3],msrow[4],1)
-    msc[j, 10:12] = h_from_n(msrow[6:8], a, b)
+    b = b.from.angles(a, msrow[3],msrow[4],1)
+    msc[j, 10:12] = h.from.n(msrow[6:8], a, b)
   }
   return(msc)
 }
